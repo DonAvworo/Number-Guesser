0fadb232ffbbf0c718fb56488e028f07571f4698
Don Avworo <theprodigaldon@icloud0.com>
2023-03-01 21:17:42 +0000

    added js

---
+++ script.js
@@ -0,0 +1,14 @@
+//create an array of numbers from 1 to 100
+
+let numbers = [];                   //create an empty array
+for (let i = 1; i <= 100; i++) {    //loop through the numbers
+    numbers.push(i);                //add the numbers to the array
+}
+
+// create a click event for the Guess button
+
+
+// create a click event for the Reset button
+
+// create a function to generate a random number
+
